d4a3741a9a1224ab74e7089e753f7ad0837e17c8
MAC <mac@MacBook-Pro-cua-MAC.local>
2020-11-05 18:33:44 +0700

    push

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    @Autowired
+    private  final ProductRepository productRepository;
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+    public void deleteById(Integer id){
+        productRepository.deleteById(id);
+    }
+    public Optional<Product> findById(Integer id){
+        return productRepository.findById(id);
+    }
+}
+
